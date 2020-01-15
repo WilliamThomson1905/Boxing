3f44729ce1a90a97e72e6590a028c4f66bd138a7
William Thomson <williamthomson@gmail.com>
2020-01-15 17:42:35 +0000

    Add project files.

---
+++ BoxingSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BoxingSite.Startup))]
+namespace BoxingSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
