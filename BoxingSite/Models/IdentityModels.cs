81f33a9fa7d40931b8e75d7ae54dce8e039a0a26
William Thomson <williamthomson@gmail.com>
2020-01-24 14:03:54 +0000

    Extended the IdentityModels classes and provided atrributes for each class. Going to move onto using EntityFramework now for a local DB.

--- BoxingSite/Models/IdentityModels.cs
+++ BoxingSite/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Security.Claims;
@@ -11,7 +12,7 @@ namespace BoxingSite.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        public string T_Id { get; set; }
+        public int ID { get; set; }
         public string Title { get; set; }
         public string Forename { get; set; }
         public string Surname { get; set; }
@@ -19,9 +20,16 @@ namespace BoxingSite.Models
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime? DOB { get; set; }
-        public char Gender { get; set; }
         public string Mobile { get; set; }
-       
+
+
+        // Has their account be hidden from other users/not admin? T: Yes -- F: No
+        public bool AccountHidden { get; set; }
+
+        public ApplicationUser()
+        {
+            AccountHidden = true; 
+        }
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
@@ -34,10 +42,24 @@ namespace BoxingSite.Models
 
     public class GeneralUser : ApplicationUser
     {
+        public SkillLevel SkillLevel { get; set; }
+        public float Weight { get; set; }
+        public float Height { get; set; }
+        public char Gender { get; set; }
 
     }
 
+    public enum SkillLevel
+    {
+        // Novice, (Advanced) Beginner, Competent, Proficient, Expert.
+        NOVICE, BEGINNER, COMPETENT, PROFICIENT, EXPERT
+    }
 
+
+
+    /// <summary>
+    /// The Trainers in the gym - each will instanciate this class 
+    /// </summary>
     public class TrainerUser : ApplicationUser
     {
         public string Description { get; set; }
@@ -49,17 +71,49 @@ namespace BoxingSite.Models
 
     }
 
-    public class AdminUser : ApplicationUser
+    /// <summary>
+    /// The Staff class will be used to store data for the following users: 
+    ///     Administrators, Staff. 
+    /// </summary>
+    public class Staff : ApplicationUser
     {
-
+        public Staff() { }
     }
 
 
     public class Equipment
     {
+        public int EquipmentID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? PurchaseDate { get; set; }
+        public float Price { get; set; }
+
+
+        // Foriegn key 
+        public int SupplierID { get; set; }
+        // Corresponding navigation property 
+        public virtual Supplier Supplier { get; set; }
 
     }
 
-    
+    // Supplier of Equipment 
+    public class Supplier
+    {
+        // Primary Key
+        public int SupplierID { get; set; }
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+
+        // Navigation property - Suppliers can provide multiple instances of 'Equipment' 
+        public virtual ICollection<Equipment> Equipments { get; set; }
+
+    }
+
+
 
 }
--- BoxingSite/Models/RoleNames.cs
+++ BoxingSite/Models/RoleNames.cs
@@ -7,11 +7,15 @@ namespace BoxingSite.Models
 
         // There will be three types of accounts (excluding locked/suspended): 
         //      Administrator - Creating Trainer Accounts and managing all the information throughout the system (equipment/schedule details/pricing/trainers details). 
+        //      Staff         - General staff members. 
         //      Trainer/Coach - Managing there own details which will be displayed on Trainer.cshtml and their own details view (trainer/trainerDetails/id).
         //      General User  - Undecided at the moment - if I implement booking functionality, then they'll be able to do that. 
-            
+
         // Admin Accounts
         public const string ROLE_ADMINISTRATOR = "Administrator";
+
+        // Staff 
+        public const string ROLE_STAFF = "Staff";
 
         // Trainer/Coach Accounts
         public const string ROLE_TRAINER = "Trainer";
